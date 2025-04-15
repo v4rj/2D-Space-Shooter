4d48bb61b91a73a44e2d1955e970a84db908816d
Andrew Varjabedian <varjabedianandrew@yahoo.com>
2025-04-15 14:26:34 -0700

    Added credit text on main menu. Gave button click functionality to start game and adjusted UI manager to restart main game scene, not main menu.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
         {
-            SceneManager.LoadScene(0); //Current Game Scene
+            SceneManager.LoadScene(1); //main game scene
         }
     }
 
---
+++ Assets/Scripts/Main_Menu/Main_Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1); //main game scene
+    }
+}
