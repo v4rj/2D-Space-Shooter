bcf5a183415f7cab03af9631943f43e90afcf8ec
Andrew Varjabedian <varjabedianandrew@yahoo.com>
2025-04-15 17:39:29 -0700

    Created explosion animation asteroid and added asteroid and explosion scripts to make sure it instantiates the explosion on asteroid destruction and destroys the explosion animation when it is finished.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 3f;
+    [SerializeField]
+    private GameObject _anim;
+
+    private Vector3 posToSpawn;
+
+    void Start()
+    {
+
+        if (_anim == null)
+        {
+            Debug.LogError("Explosion Animation animator is null");
+        }
+    }
+
+    void Update()
+    {
+        posToSpawn = transform.position;
+
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+
+            Instantiate(_anim, posToSpawn, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+    }
+}
